f3907601128493c68f79099de85c9ded353deaf0
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-04-22 10:24:12 -0600

    Create FancyBoxThumbsHooks.php

---
+++ FancyBoxThumbsHooks.php
@@ -0,0 +1,13 @@
+class FancyBoxThumbsHooks {
+	/**
+	 * @param OutputPage $out
+	 * @param Skin $skin
+	 */
+	public static function onBeforePageDisplay( OutputPage $out, Skin $skin ) {
+		global $wgFancyBoxThumbsOptions;
+
+		$out->addModules( 'ext.FancyBoxThumbs' );
+
+		$out->addInlineScript( "var wgFancyBoxThumbsOptions = {$wgFancyBoxThumbsOptions};" );
+	}
+}
